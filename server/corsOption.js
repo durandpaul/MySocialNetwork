604e0bb2b95c3be00d2ad2471d36e8d5a16e88e1
durand_p <paul.a.durand@outlook.fr>
2019-07-31 00:56:51 +0200

    ...

--- server/corsOption.js
+++ server/corsOption.js
@@ -1,5 +1,5 @@
 module.exports = {
-    origin: 'http://localhost:8080',
+    origin: 'https://connecturth.herokuapp.com/',
     optionsSuccessStatus: 200,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: false

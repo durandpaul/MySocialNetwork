439cabc8e1a9dcd303a08f2eb9b26547941cf720
Paul <paul.a.durand@outlook.fr>
2019-07-31 13:02:14 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -28,9 +28,9 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname +  '/dist'));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
 
 const api = require('./routes/index');
 
